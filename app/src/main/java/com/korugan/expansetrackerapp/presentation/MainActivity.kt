6b7332447b02ab658148f8242291565e3d091463
Korugan32 <tahacansenel@gmail.com>
2024-11-03 20:27:50 +0300

    Create Presentation Package

--- app/src/main/java/com/korugan/expansetrackerapp/presentation/MainActivity.kt
+++ app/src/main/java/com/korugan/expansetrackerapp/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.korugan.expansetrackerapp
+package com.korugan.expansetrackerapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.korugan.expansetrackerapp.ui.theme.ExpanseTrackerAppTheme
+import com.korugan.expansetrackerapp.presentation.common.navigation.Navigation
+import com.korugan.expansetrackerapp.presentation.ui.theme.ExpanseTrackerAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ExpanseTrackerAppTheme {
-
+                Navigation()
             }
         }
     }
--- app/src/main/java/com/korugan/expansetrackerapp/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/korugan/expansetrackerapp/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.korugan.expansetrackerapp.ui.theme
+package com.korugan.expansetrackerapp.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
