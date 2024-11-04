5012b7817c87e38ca67478ce84d03bd9a4f6748d
Korugan32 <tahacansenel@gmail.com>
2024-11-04 13:16:29 +0300

    Implementation Room Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -65,6 +66,9 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     //Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
     //Dependency Injection
 
