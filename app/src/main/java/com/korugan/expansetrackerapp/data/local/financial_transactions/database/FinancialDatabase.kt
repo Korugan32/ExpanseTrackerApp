6ebacd1e45dfc896a15b6664d9cc868aada4302f
Korugan32 <tahacansenel@gmail.com>
2024-11-04 13:43:56 +0300

    Create FinancialDatabase

---
+++ app/src/main/java/com/korugan/expansetrackerapp/data/local/financial_transactions/database/FinancialDatabase.kt
@@ -0,0 +1,30 @@
+package com.korugan.expansetrackerapp.data.local.financial_transactions.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.korugan.expansetrackerapp.data.local.asset_inventory.entity.Assets
+import com.korugan.expansetrackerapp.data.local.financial_transactions.dao.FinancialDao
+
+
+@Database(entities = [Assets::class], version = 1, exportSchema = false)
+abstract class FinancialDatabase : RoomDatabase() {
+    abstract fun financialDao(): FinancialDao
+    companion object {
+        @Volatile
+        private var INSTANCE: FinancialDatabase? = null
+
+        fun getDatabase(context: Context): FinancialDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FinancialDatabase::class.java,
+                    "financial_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
