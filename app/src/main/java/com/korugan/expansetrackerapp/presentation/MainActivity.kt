c5a62c645d681912cec5277b2a9f3d0ee46b3b6f
Korugan32 <tahacansenel@gmail.com>
2024-11-04 11:26:35 +0300

    Update Theme

--- app/src/main/java/com/korugan/expansetrackerapp/presentation/MainActivity.kt
+++ app/src/main/java/com/korugan/expansetrackerapp/presentation/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.korugan.expansetrackerapp.presentation.common.navigation.Navigation
-import com.korugan.expansetrackerapp.presentation.ui.theme.ExpanseTrackerAppTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+
+import com.korugan.expansetrackerapp.presentation.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ExpanseTrackerAppTheme {
-                Navigation()
+            AppTheme {
+                Column(modifier = Modifier.background(MaterialTheme.colorScheme.background).fillMaxSize()){
+
+                }
             }
         }
     }
