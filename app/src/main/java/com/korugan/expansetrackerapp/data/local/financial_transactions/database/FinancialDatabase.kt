15d4b1b16ee24d55f62c7dec64c747cd28a9dda9
Korugan32 <tahacansenel@gmail.com>
2024-11-11 18:53:50 +0300

    Fix FinancialDatabase

--- app/src/main/java/com/korugan/expansetrackerapp/data/local/financial_transactions/database/FinancialDatabase.kt
+++ app/src/main/java/com/korugan/expansetrackerapp/data/local/financial_transactions/database/FinancialDatabase.kt
@@ -5,9 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.korugan.expansetrackerapp.data.local.financial_transactions.dao.FinancialDao
+import com.korugan.expansetrackerapp.data.local.financial_transactions.entity.Financials
 
 
-@Database(entities = [FinancialDatabase::class], version = 1, exportSchema = false)
+@Database(entities = [Financials::class], version = 1, exportSchema = false)
 abstract class FinancialDatabase : RoomDatabase() {
     abstract fun financialDao(): FinancialDao
     companion object {
