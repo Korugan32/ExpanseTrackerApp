70b06e3ac9d692d333d535b10a35f38d923fe738
Korugan32 <tahacansenel@gmail.com>
2024-11-19 21:19:13 +0300

    Create GetNegativeFinancialUseCase

---
+++ app/src/main/java/com/korugan/expansetrackerapp/domain/usecase/financial/GetNegativeFinancialUseCase.kt
@@ -0,0 +1,14 @@
+package com.korugan.expansetrackerapp.domain.usecase.financial
+
+import com.korugan.expansetrackerapp.data.local.financial_transactions.entity.Financials
+import com.korugan.expansetrackerapp.domain.repository.financial.FinancialRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetNegativeFinancialUseCase @Inject constructor(
+    private val financialRepository: FinancialRepository
+) {
+    operator fun invoke(): Flow<List<Financials>> {
+        return financialRepository.getNegativeFinancials()
+    }
+}
