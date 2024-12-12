49f8bbebe36f4941feef8c33f09f9c8ede6992e9
Korugan32 <tahacansenel@gmail.com>
2024-12-13 00:42:36 +0300

    Create StockData

---
+++ app/src/main/java/com/korugan/expansetrackerapp/data/remote/stock/data/dto/Data.kt
@@ -0,0 +1,9 @@
+package com.korugan.expansetrackerapp.data.remote.stock.data.dto
+
+data class Data(
+    val ccI14: Any,
+    val hisseYuzeysel: HisseYuzeysel,
+    val mov10: Any,
+    val rsI14: Any,
+    val stc_5_3: Any
+)
