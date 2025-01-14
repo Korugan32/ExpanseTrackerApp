94cfc51f36ff8d0fc0e4e0ccf1d0b6da756a327e
Korugan32 <tahacansenel@gmail.com>
2025-01-14 04:33:14 +0300

    Update NewsComponent

--- app/src/main/java/com/korugan/expansetrackerapp/presentation/common/components/NewsComponent.kt
+++ app/src/main/java/com/korugan/expansetrackerapp/presentation/common/components/NewsComponent.kt
@@ -1,12 +1,12 @@
 package com.korugan.expansetrackerapp.presentation.common.components
 
-import androidx.compose.foundation.Image
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -21,73 +21,71 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 
 @Composable
-fun NewsCard() {
+fun NewsCard(title: String, image: String, source: String, sourceUrl: String) {
+    val context = LocalContext.current
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(12.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-
     ) {
         Box(
             modifier = Modifier
                 .clip(RoundedCornerShape(20.dp))
                 .fillMaxWidth()
-                .size(340.dp)
-                .background(color = Color.LightGray),
-
+                .size(250.dp)
+                .background(color = Color.LightGray)
+                .clickable {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(sourceUrl))
+                    context.startActivity(intent)
+                },
         ) {
-            Image(painter = painterResource(R.drawable.news),
+            AsyncImage(
+                model = image,
                 contentDescription = "",
                 modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop)
-
+                contentScale = ContentScale.Crop
+            )
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(130.dp) // Gölge ve başlık için ayrılan yükseklik
-                    .align(Alignment.BottomCenter) // Resmin altına hizalar
+                    .height(130.dp)
+                    .align(Alignment.BottomCenter)
                     .background(
                         Brush.verticalGradient(
-                            colors = listOf(Color.Transparent, Color.Black.copy(alpha = 0.7f)),
+                            colors = listOf(Color.Transparent, Color.Black.copy(alpha = 0.6f)),
                             startY = 0f,
                             endY = 100f
                         )
                     )
             )
-
-            Text(text = "Türkiye’yi Yasa Boğan Deprem: Maraş Merkezli Felaket ",
+            Text(
+                text = title,
                 fontSize = 18.sp,
                 color = Color.White,
                 overflow = TextOverflow.Ellipsis,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.BottomStart)
-                    .padding(24.dp,),
-
-                )
-
-            Text(text = "CNN Türk •Bugün",
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(24.dp)
+            )
+            Text(
+                text = source,
                 fontSize = 16.sp,
                 color = Color.White,
-                modifier = Modifier.align(Alignment.BottomEnd)
-                    .padding(8.dp))
-
-
-
-
-
-
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(8.dp)
+            )
         }
-
     }
-
-
 }
