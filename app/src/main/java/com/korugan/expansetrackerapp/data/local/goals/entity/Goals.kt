b140c6c3753aee3397db815ba7370a763a29ad43
Korugan32 <tahacansenel@gmail.com>
2025-04-22 12:50:12 +0300

    Create Goals

---
+++ app/src/main/java/com/korugan/expansetrackerapp/data/local/goals/entity/Goals.kt
@@ -0,0 +1,17 @@
+package com.korugan.expansetrackerapp.data.local.goals.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "goals")
+data class Goals (
+    val title : String,
+    val description : String,
+    val deadline : Date,
+    val progress : Int,
+    val status : String,
+    val price : Double,
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0
+)
