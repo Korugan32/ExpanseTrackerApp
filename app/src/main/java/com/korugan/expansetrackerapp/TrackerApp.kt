9bb58a8ee31f6fbebc7ee22bfd543a0e52212d1c
Korugan32 <tahacansenel@gmail.com>
2025-05-19 17:49:14 +0300

    Integrate Hilt with WorkManager

--- app/src/main/java/com/korugan/expansetrackerapp/TrackerApp.kt
+++ app/src/main/java/com/korugan/expansetrackerapp/TrackerApp.kt
@@ -1,7 +1,20 @@
 package com.korugan.expansetrackerapp
 
 import android.app.Application
+import androidx.work.Configuration
+import androidx.hilt.work.HiltWorkerFactory
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class TrackerApp : Application()
+class TrackerApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
+
