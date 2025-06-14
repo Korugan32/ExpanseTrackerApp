f86a34445c199bbe9a2cdfa61df8ed434ddbc06c
Taha Can Şenel <147408850+Korugan32@users.noreply.github.com>
2025-06-14 04:52:59 +0300

    Refactor Transaction composable to use ElevatedCard

--- app/src/main/java/com/korugan/expansetrackerapp/presentation/common/components/Transaction.kt
+++ app/src/main/java/com/korugan/expansetrackerapp/presentation/common/components/Transaction.kt
@@ -3,7 +3,6 @@ package com.korugan.expansetrackerapp.presentation.common.components
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,10 +12,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -28,7 +28,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -53,24 +52,24 @@ fun Transaction(
     val formattedDate = formatter.format(date)
     var expanded by remember { mutableStateOf(false) }
     val cardHeight by animateDpAsState(
-        targetValue = if (expanded) 120.dp else 80.dp,
+        targetValue = if (expanded) 120.dp else 70.dp,
         label = "ExpandAnimation"
     )
     val rotation by animateFloatAsState(
         targetValue = if (expanded) 180f else 0f,
         label = "RotationAnimation"
     )
-    Column(
+    ElevatedCard(
         Modifier
             .fillMaxWidth()
-            .clip(RoundedCornerShape(15.dp))
-            .background(MaterialTheme.colorScheme.onPrimary)
-            .clickable { expanded = !expanded }
             .height(cardHeight)
-            .padding(15.dp),
+            .padding(horizontal = 10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary
+        ),
     ) {
         Row(
-            Modifier.fillMaxWidth(),
+            Modifier.fillMaxWidth().clickable { expanded = !expanded }.padding(15.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -106,7 +105,7 @@ fun Transaction(
         }
         if (expanded) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 15.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
